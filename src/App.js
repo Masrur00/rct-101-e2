47ede0acbdd27313d1daf28d29680cef48d04279
Masrur Alam <masrur.jsr@gmail.com>
2022-05-30 14:15:54 +0530

    just display purpose

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const App = () => {
-  return <div>{/* TODO: Code here */}</div>;
+  return <div>{/* TODO: Code here */}
+  <h2>My name is khan</h2>
+  </div>;
 };
 
 export default App;
